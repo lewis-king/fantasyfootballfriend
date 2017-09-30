1fd553b56a8ce9613ca0501d5a98d07c29698758
Lewis King <lking1221@gmail.com>
2017-09-30 12:57:02 +0100

    Add same conditional class logic to all Net (+/-) columns for dynamically changing the css colour

--- client/components/trending.js
+++ client/components/trending.js
@@ -71,9 +71,9 @@ class Trending extends Component {
                             <td className="trending-table" colSpan="1">{player.nowTransfersIn}</td>
                             <td className="trending-table" colSpan="1">{player.nowTransfersOut}</td>
                             <td className={"trending-table " + ((player.netTransfers >= 0) ? 'pos' : 'neg')} colSpan="1">{player.netTransfers}</td>
-                            <td className={((player.transferFeeCostChangeStart > 0) ? 'pos' : 'neg')}><NumberFormat value={player.transferFeeCostChangeStart} displayType={'text'}
+                            <td className={"trending-table " + ((player.transferFeeCostChangeStart >= 0) ? 'pos' : 'neg')} colSpan="1"><NumberFormat value={player.transferFeeCostChangeStart} displayType={'text'}
                                               thousandSeparator={true} prefix={'£'}/></td>
-                            <td className={((player.transferFeeCostChangeStart > 0) ? 'pos' : 'neg')}><NumberFormat value={player.transferFeeCostChangeForGW} displayType={'text'}
+                            <td className={"trending-table " + ((player.transferFeeCostChangeStart >= 0) ? 'pos' : 'neg')} colSpan="1"><NumberFormat value={player.transferFeeCostChangeForGW} displayType={'text'}
                                               thousandSeparator={true} prefix={'£'}/></td>
                         </tr>
                     ))}
